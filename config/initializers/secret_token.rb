43c96fcdc05609c17976db96d3dd279cde144d2e
Cholthon Klingosum <ink@love.com>
2013-02-05 05:20:37 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoRTutEx1::Application.config.secret_token = '675f2e3a16b1273f2101a1be9f38cd8a77f3d88e4474d389fd1fc593ecc3d8e394cdbadcc59eb76ede26cc97776ca10bb85f0a8b1e71a6c96eac12d3cc1790e6'
